c3a5f2b287208258668c56c4b7af75c951619877
Kwame Oduro <odurokwameee@gmail.com>
2025-09-29 06:57:35 +0000

    test: update order step definition

--- src/test/java/com/ecommerce/steps/OrderSteps.java
+++ src/test/java/com/ecommerce/steps/OrderSteps.java
@@ -1,60 +1,65 @@
 package com.ecommerce.steps;
 
+import com.ecommerce.pages.CartPage;
+import com.ecommerce.pages.HomePage;
 import com.ecommerce.pages.OrderPage;
-import io.cucumber.datatable.DataTable;
+import com.ecommerce.pages.ProductPage;
 import io.cucumber.java.en.*;
+import org.openqa.selenium.Alert;
 
-import java.util.Map;
-
+import static com.codeborne.selenide.Selenide.switchTo;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderSteps {
+
+    private final HomePage homePage = new HomePage();
+    private final CartPage cartPage = new CartPage();
     private final OrderPage orderPage = new OrderPage();
+    private final ProductPage productPage = new ProductPage();
 
-    @Given("the customer clicks Place Order")
-    public void clickPlaceOrder() {
-        assertTrue(orderPage.isOrderModalDisplayed(),
-                "Order modal should be visible after clicking Place Order");
+
+    @Given("the customer has 1 item in the cart")
+    public void customerHasItemInCart() {
+        homePage.clickProductByName("Samsung galaxy s6");   // opens details page
+        productPage.clickAddToCartt();                       // clicks add to cart
+        homePage.acceptAlert();                             // accept JS alert
+        homePage.clickCart();                               // go to cart page
+        assertTrue(cartPage.isCartTableVisible(), "Cart page should be visible with item inside");
     }
 
-    @When("the customer enters order details")
-    public void enterOrderDetails(DataTable dataTable) {
-        Map<String, String> data = dataTable.asMap(String.class, String.class);
-
-        orderPage.enterName(data.get("name"));
-        orderPage.enterCountry(data.get("country"));
-        orderPage.enterCity(data.get("city"));
-        orderPage.enterCard(data.get("card"));
-        orderPage.enterMonth(data.get("month"));
-        orderPage.enterYear(data.get("year"));
+    @When("the customer opens the order form")
+    public void openOrderForm() {
+        orderPage.clickPlaceOrderFromCart();
+        assertTrue(orderPage.isOrderModalDisplayed(), "Order modal should be displayed");
     }
 
-    @When("the customer clicks Purchase")
-    public void clickPurchase() {
+    @When("the customer enters name {string} and credit card {string}")
+    public void enterNameAndCard(String name, String cc) {
+        orderPage.enterName(name);
+        orderPage.enterCard(cc);
+    }
+
+    @When("the customer submits the order")
+    public void submitOrder() {
         orderPage.clickPurchase();
     }
 
-    @Then("the success modal should be displayed")
-    public void successModalDisplayed() {
+    @Then("the order confirmation should be displayed")
+    public void confirmationDisplayed() {
         assertTrue(orderPage.isSuccessModalDisplayed(),
-                "Success modal should be displayed after purchase");
-    }
-
-    @Then("the success title should be {string}")
-    public void verifySuccessTitle(String expected) {
-        assertEquals(expected, orderPage.getSuccessTitle(),
-                "Success modal title should match");
-    }
-
-    @Then("the success details should not contain the card details {string}")
-    public void verifyCardNotDisplayed(String cardNumber) {
-        String details = orderPage.getSuccessDetails();
-        assertFalse(details.contains(cardNumber),
-                "Card number should not appear in success details!");
-    }
-
-    @Then("the customer clicks OK")
-    public void clickOk() {
+                "Success modal should be displayed after placing order");
+        assertEquals("Thank you for your purchase!", orderPage.getSuccessTitle(),
+                "Order success message should match");
         orderPage.clickOkButton();
+    }
+
+    @Then("an error message should be shown")
+    public void errorMessageShown() {
+        // Demoblaze shows a JavaScript alert when required fields are missing
+        Alert alert = switchTo().alert();
+        String alertText = alert.getText();
+        assertTrue(alertText.toLowerCase().contains("please fill"),
+                "Expected error message about missing credit card, but got: " + alertText);
+        alert.accept();
     }
 }
